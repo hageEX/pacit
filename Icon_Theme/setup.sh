c7a720f7eec1eabfe5c7a5fde5f58241991643cf
hageEX <gangangandon@icloud.com>
2019-07-23 22:23:54 +0900

    icon & theme

--- Icon_Theme/setup.sh
+++ Icon_Theme/setup.sh
@@ -2,6 +2,7 @@
 
 sudo mkdir -p /usr/local/share/pixmaps
 sudo cp -u vender/parrot.svg vender/parrot-logo.png /usr/local/share/pixmaps
+sudo cp -u vender/parrot-icons/* /usr/local/share/pixmaps
 
 gsettings set org.mate.panel.menubar icon-name parrot
 #gsettings set org.mate.panel.menubar icon-name parrot-logo
