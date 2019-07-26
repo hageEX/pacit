8d8e59b5c477ddddecc790bd2b4d0bedd9774c79
hageEX <gangangandon@icloud.com>
2019-07-26 12:24:37 +0900

    parrot-conky

--- conky/setup_conky.sh
+++ conky/setup_conky.sh
@@ -4,29 +4,16 @@ mkdir -p ~/.config/conky
 sudo mkdir -p /etc/conky
 sudo mkdir -p /usr/share/fonts/truetype
 
-echo -n "setup[Y/n]: "
-
-read keyboard
-case $keyboard in
-    "" | [Yy]* )
-        sudo cp -u parrot.png rings.lua /etc/conky
-        sudo cp -u conky-start.desktop /usr/share/applications
-        sudo cp -ru future /usr/share/fonts/truetype
-        cp -u conky.conf ~/.config/conky
-        cp -u conky-start.desktop ~/Desktop
-        echo "Setup to ~/.config/conky/conky.conf"
-        echo "Setup to /etc/conky/parrot.png"
-        echo "Setup to /etc/conky/rings.lua"
-        echo "Setup to /usr/share/applications/conky-start.desktop"
-        echo "Setup to ~/Desktop/conky-start.desktop"
-        echo "Setup to /usr/share/fonts/trutype/future"
-        sudo apt install conky conky-all
-        conky &
-        echo "Complete."
-        ;;
-    * )
-        echo "Thank you. Bye."
-        ;;
-esac
-
-
+sudo cp -u parrot.png rings.lua /etc/conky
+sudo cp -u conky-start.desktop /usr/share/applications
+sudo cp -ru future /usr/share/fonts/truetype
+cp -u conky.conf ~/.config/conky
+cp -u conky-start.desktop ~/Desktop
+echo "Setup to ~/.config/conky/conky.conf"
+echo "Setup to /etc/conky/parrot.png"
+echo "Setup to /etc/conky/rings.lua"
+echo "Setup to /usr/share/applications/conky-start.desktop"
+echo "Setup to ~/Desktop/conky-start.desktop"
+echo "Setup to /usr/share/fonts/trutype/future"
+sudo apt -y install conky conky-all
+conky &
